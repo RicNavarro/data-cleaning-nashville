65a08748724272cb44a95df79abba2f481bd161c
Ricardo <ricardonavarro2611@gmail.com>
2024-04-23 00:17:16 -0300

    Arquivo de queries adicionado
    Criado e adicionado o arquivo .sql onde as queries para a limpeza dos dados serão realizadas

---
+++ CleaningFile.sql
@@ -0,0 +1,65 @@
+/*
+
+Limpando os dados com SQL
+
+*/
+
+Select *
+From PortifolioProject..NashvilleHousing
+
+-----------------------------------------------------------------
+
+-- Padronizando o formato das datas
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-- Populando os dados de endereço de cada imóvel (PropretyAddress)
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-- Divivindo o campo de endereço (PropertyAddress) em colunas individuais (Address, City, State)
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-- Mudar o Y e N no campo "SoldAsVacant" para Yes e No
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-- Remover duplicatas
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-- Deletar colunas não usadas
+
+
+
+
+
+
+-----------------------------------------------------------------
+
