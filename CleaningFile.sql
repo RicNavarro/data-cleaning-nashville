28b53a9267b8bbcd2ad724f3bf3b68785fa862f7
Ricardo <ricardonavarro2611@gmail.com>
2024-05-08 12:04:36 -0300

    Sem mais Null em PropertyAddress
    Foi feita uma comparação entre o campo ParcelID e o campo PropertyAddress, de modo a popular as entradas onde o campo PropertyAddress estava como 'Null' ao invés do endereço correto

--- CleaningFile.sql
+++ CleaningFile.sql
@@ -38,9 +38,47 @@ SET SaleDateConverted = CONVERT(Date, SaleDate)
 
 --		 Populando os dados de endereço de cada imóvel (PropretyAddress)
 
+-- Realizando uma primeira vizualização e vendo que muitas entradas têm o endereço
+-- da propriedade definido como nulo
+Select *
+From PortifolioProject..NashvilleHousing
+Where PropertyAddress is Null
 
+-- Algo que podemos fazer é observar o ParcelID, pois este indica
+-- um único endereço, isto é, um único PropertyAddress
+Select ParcelID, PropertyAddress
+From PortifolioProject..NashvilleHousing
+ORDER BY ParcelID
 
+-- Usando um Join da tabela com ela mesma (entre 'a' e 'b'), verificamos as
+-- entradas distintas(UniqueID distintos) onde os ParcelID são os mesmos. Também
+-- selecionamos os endereços(PropertyAddress) para vizualizar os locais onde há um 'Null'
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress
+From PortifolioProject..NashvilleHousing a
+JOIN PortifolioProject..NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID] <> b.[UniqueID]
+WHERE a.PropertyAddress IS Null
 
+-- Ulilizando 'ISNULL' na query acima, podemos selecionar o que virá a
+-- popular as entradas com PropertyAddress = Null, ao fazer equivalência
+-- entre o que temos em a.PropertyAddress com o campo correto de b.PropertyAddress
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortifolioProject..NashvilleHousing a
+JOIN PortifolioProject..NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID] <> b.[UniqueID]
+WHERE a.PropertyAddress IS Null
+
+-- Atualizando a tabela própriamente dita.
+-- Agora nenhuma entrada tem PropertyAddress como Null
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortifolioProject..NashvilleHousing a
+JOIN PortifolioProject..NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID] <> b.[UniqueID]
+WHERE a.PropertyAddress IS Null
 
 
 -----------------------------------------------------------------
