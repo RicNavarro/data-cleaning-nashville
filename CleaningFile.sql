6695660dbf00dd667981fed01baa0c7ff3837d28
Ricardo <ricardonavarro2611@gmail.com>
2024-05-08 11:45:33 -0300

    Tipo de SaleDate foi corrigido
    SaleDate era do tipo 'nvarchar' e agora é do tipo 'Date'

--- CleaningFile.sql
+++ CleaningFile.sql
@@ -1,6 +1,6 @@
 /*
 
-Limpando os dados com SQL
+		Limpando os dados com SQL
 
 */
 
@@ -9,16 +9,34 @@ From PortifolioProject..NashvilleHousing
 
 -----------------------------------------------------------------
 
--- Padronizando o formato das datas
+--		Padronizando o formato das datas
 
+-- Observando o formato de SaleDate
+Select SaleDate
+From PortifolioProject..NashvilleHousing
 
+-- Realizando uma conversão para vizualisar como SaleDate fica ao ser convertida
+-- pro formato Date, deixando de ser nvarchar
+Select SaleDate, CONVERT(Date, SaleDate)
+From PortifolioProject..NashvilleHousing
 
+-- Atualizando a tabela com SaleDate agora com o formato Date
+UPDATE NashvilleHousing
+SET SaleDate = CONVERT(Date, SaleDate)
 
+/*
+Forma alternativa de atualizar
 
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date, SaleDate)
+*/
 
 -----------------------------------------------------------------
 
--- Populando os dados de endereço de cada imóvel (PropretyAddress)
+--		 Populando os dados de endereço de cada imóvel (PropretyAddress)
 
 
 
